88b789c496aca16807067957855970ead711cee5
Shahed Chowdhury <shahed@Shaheds-MacBook-Air.local>
2022-01-15 02:37:49 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const cors = require("cors");
+const app = express();
+
+// DB= mongodb+srv://shahed:shahed123@cluster0.juwqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+app.use(cors());
+mongoose.connect(
+  "mongodb+srv://shahed:shahed123@cluster0.juwqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
+mongoose.connection.once("open", () => console.log("Database connected"));
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
